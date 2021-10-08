5c42ddd7fa88ee831d5d7423ed9d5134feda87d4
prashishb <prashb.web@gmail.com>
2021-10-08 11:36:29 -0400

    creating accordion using styled-components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div className="App">
-      {characters.map(elem => <Character character={elem} />)}
+      {characters.map(elem => <Character character={elem} />)};
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -7,9 +7,12 @@ export default function Character(props) {
     const {character} = props;
 
     return (
-        <div>
-            <h1>{character.name}</h1>
-            <div class="character-info">
+        <StyledAccordion>
+            <button className="accordion">
+                <h1 className="accordion-title">{character.name}</h1>
+                <h1 className="accordion-title">{character.birth_year}</h1>
+            </button>
+            <div class="accordion-content">
                 <p>Gender: {character.gender}</p>
                 <p>Height: {character.height}</p>
                 <p>Mass: {character.mass}</p>
@@ -18,6 +21,31 @@ export default function Character(props) {
                 <p>Hair Color: {character.hair_color}</p>
                 <p>Skin Color: {character.skin_color}</p>
             </div>
-        </div>
+        </StyledAccordion>
     )
 }
+
+const StyledAccordion = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .accordion {
+        background-color: #eee;
+        color: #444;
+        cursor: pointer;
+        padding: 18px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border: none;
+        outline: none;
+        transition: background-color 0.6s ease;
+        &:hover, .active {
+            background-color: #ccc;
+        }
+    }
+
+    .accordion-title {
+        font-size: 20px;
+    }
+`
