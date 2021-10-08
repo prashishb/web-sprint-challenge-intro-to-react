c5987b5cfdb6e722f485c8e250f247665ced2e77
prashishb <prashb.web@gmail.com>
2021-10-08 10:19:35 -0400

    rendered characters and their infos

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function Character(props) {
+    const {character} = props;
+
+    return (
+        <div>
+            <h1>{character.name}</h1>
+            <div class="character-info">
+                <p>Gender: {character.gender}</p>
+                <p>Height: {character.height}</p>
+                <p>Mass: {character.mass}</p>
+                <p>BirthYear: {character.birth_year}</p>
+                <p>Eye Color: {character.eye_color}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Skin Color: {character.skin_color}</p>
+            </div>
+        </div>
+    )
+}
